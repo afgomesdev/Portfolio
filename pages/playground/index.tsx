b83a7f0909e891bc8cf2adf8b7994af52ecef96d
André Felipe Benedetti Gomes <andregomes@pecege.com>
2021-11-15 19:15:15 -0300

    fix: add menu and footer

--- pages/playground/index.tsx
+++ pages/playground/index.tsx
@@ -1,11 +1,15 @@
+import Footer from "../../components/Footer";
 import Lighsaber from "../../components/Lighsaber";
+import Menu from "../../components/Menu";
 import { Container, Content } from "./styles";
 export function Playground(): JSX.Element {
   return (
     <Container>
+      <Menu />
       <Content>
         <Lighsaber />
       </Content>
+      <Footer />
     </Container>
   );
 }
